2c7369389b7de057880c98fbfbfcf724add7436b
Alec McGavin <alec.mcgavin@kogan.com.au>
2021-12-08 20:11:32 +1100

    I'll add some comments

--- 2021/python/08.py
+++ 2021/python/08.py
@@ -32,15 +32,33 @@ def part_2(data):
         source, _, output = line.partition(" | ")
         source = source.split(" ")
         output = output.split(" ")
+
+        # length of the segments reveals unique number sets
         digits = sorted(source, key=lambda s: (len(s)))
+
+        # frequency analysis reveals unique segments
         counts = [c[0] for c in Counter("".join(source)).most_common()]
         key = {}
+
+        # bottom right
         key["f"] = counts[0]
+
+        # top left
         key["b"] = counts[5]
+
+        # bottom left
         key["e"] = counts[6]
+
+        # `a` (top) is in `7` but not in `1`
         key["a"] = next(c for c in digits[1] if c not in digits[0])
+
+        # `c` (top right) is the 2nd most common (and not `a`)
         key["c"] = next(c for c in counts[1:3] if c not in key.values())
+
+        # `4` contains `d` (middle) but not `g`
         key["d"] = next(c for c in digits[2] if c not in key.values())
+
+        # `g` (bottom) is whatever is left
         key["g"] = next(c for c in digits[-1] if c not in key.values())
         key = {value: key for key, value in key.items()}
 
