5451f75af6390ad7abfbc012830484b428751d13
Alec McGavin <alec.mcgavin@kogan.com.au>
2022-12-01 16:21:01 +1100

    2022/01

--- 2022/python/01.py
+++ 2022/python/01.py
@@ -2,15 +2,31 @@ import aocd
 
 
 def part_1(data):
-    return
+    groups = []
+    total = 0
+    for n in data:
+        if n == "":
+            groups.append(total)
+            total = 0
+        else:
+            total += int(n)
+    return max(groups)
 
 
 def part_2(data):
-    return
+    groups = []
+    total = 0
+    for n in data:
+        if n == "":
+            groups.append(total)
+            total = 0
+        else:
+            total += int(n)
+    return sum(sorted(groups)[-3:])
 
 
 def main():
-    data = [int(x) for x in aocd.get_data(day=1, year=2022).splitlines()]
+    data = [x for x in aocd.get_data(day=1, year=2022).splitlines()]
     print(part_1(data))
     print(part_2(data))
 
