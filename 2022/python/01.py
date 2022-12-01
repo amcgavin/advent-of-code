71e0f9c0b5d31ef1fb17f53b03d526934505c539
Alec McGavin <alec.mcgavin@kogan.com.au>
2022-12-01 11:49:18 +1100

    prepare for 2022

---
+++ 2022/python/01.py
@@ -0,0 +1,19 @@
+import aocd
+
+
+def part_1(data):
+    return
+
+
+def part_2(data):
+    return
+
+
+def main():
+    data = [int(x) for x in aocd.get_data(day=1, year=2022).splitlines()]
+    print(part_1(data))
+    print(part_2(data))
+
+
+if __name__ == "__main__":
+    main()
