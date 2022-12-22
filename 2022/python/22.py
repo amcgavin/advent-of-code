a4ada6ea17bd478c767b528b1b2f002b605857fe
Alec McGavin <alec.mcgavin@kogan.com.au>
2022-12-23 07:06:55 +1100

    clean it up a bit

--- 2022/python/22.py
+++ 2022/python/22.py
@@ -109,21 +109,32 @@ def part_2(data):
     steps = [int(x) for x in re.split(r"L|R", path)]
     rotations = [x for x in re.split(r"\d+", path) if x]
 
-    y_bounds = {
-        y: (min(a for a, b in grid.keys() if b == y), max(a for a, b in grid.keys() if b == y))
-        for y in range(0, max(b for a, b in grid.keys()) + 1)
+    faces = {(1, 0): 2, (2, 0): 1, (1, 1): 3, (0, 2): 5, (1, 2): 4, (0, 3): 6}
+    transforms = {
+        (1, 0): lambda a, b: (99, 149 - b % 50, 180),  # 1 > 4 <
+        (1, 90): lambda a, b: (99, 50 + a % 50, 180),  # 1 v 3 <
+        (1, 270): lambda a, b: (a - 100, 199, 270),  # 1 ^ 6 ^
+        (2, 180): lambda a, b: (0, 149 - b % 50, 0),  # 2 < 5 >
+        (2, 270): lambda a, b: (0, 150 + a % 50, 0),  # 2 ^ 6 >
+        (3, 0): lambda a, b: (100 + b % 50, 49, 270),  # 3 > 1 ^
+        (3, 180): lambda a, b: (b % 50, 100, 90),  # 3 < 5 v
+        (4, 0): lambda a, b: (149, 49 - b % 50, 180),  # 4 > 1 <
+        (4, 90): lambda a, b: (49, 150 + a % 50, 180),  # 4 v 6 <
+        (5, 180): lambda a, b: (50, 49 - b % 50, 0),  # 5 < 2 >
+        (5, 270): lambda a, b: (50, 50 + a % 50, 0),  # 5 ^ 3 >
+        (6, 0): lambda a, b: (50 + b % 50, 149, 270),  # 6 > 4 ^
+        (6, 90): lambda a, b: (100 + a, 0, 90),  # 6 v 1 v
+        (6, 180): lambda a, b: (50 + b % 50, 0, 90),  # 6 < 2 v
     }
 
-    faces = {(1, 0): 2, (2, 0): 1, (1, 1): 3, (0, 2): 5, (1, 2): 4, (0, 3): 6}
-
-    x, y = (y_bounds[0][0], 0)
+    x, y = (min(a for a, b in grid.keys() if b == 0), 0)
     angle = 0
     for instruction in itertools.chain.from_iterable(itertools.zip_longest(steps, rotations)):
         match instruction:
             case int(steps):
-                dx = int(math.cos(math.radians(angle)))
-                dy = int(math.sin(math.radians(angle)))
                 for _ in range(steps):
+                    dx = int(math.cos(math.radians(angle)))
+                    dy = int(math.sin(math.radians(angle)))
                     target = grid.get((x + dx, y + dy))
                     if target is True:
                         x += dx
@@ -132,90 +143,10 @@ def part_2(data):
                         break
                     elif target is None:
                         face = faces[x // 50, y // 50]
-                        if face == 1:
-                            if angle == 0:
-                                # 1 >> 4 <<
-                                wrapx = 99
-                                wrapy = 149 - y % 50
-                                wrapangle = 180
-                            elif angle == 90:
-                                # 1 vv 3 <<
-                                wrapx = 99
-                                wrapy = x % 50 + 50
-                                wrapangle = 180
-                            elif angle == 270:
-                                # 1 ^^ 6 ^^
-                                wrapx = x - 100
-                                wrapy = 199
-                                wrapangle = 270
-                        elif face == 2:
-                            if angle == 180:
-                                # 2 << 5 >>
-                                wrapx = 0
-                                wrapy = 149 - y % 50
-                                wrapangle = 0
-                            elif angle == 270:
-                                # 2 ^^ 6 >>
-                                wrapx = 0
-                                wrapy = x % 50 + 150
-                                wrapangle = 0
-                        elif face == 3:
-                            if angle == 0:
-                                # 3 >> 1 ^^
-                                wrapx = y % 50 + 100
-                                wrapy = 49
-                                wrapangle = 270
-                            elif angle == 180:
-                                # 3 << 5 vv
-                                wrapx = y % 50
-                                wrapy = 100
-                                wrapangle = 90
-                        elif face == 4:
-                            if angle == 0:
-                                # 4 >> 1 <<
-                                wrapx = 149
-                                wrapy = 49 - y % 50
-                                wrapangle = 180
-                            elif angle == 90:
-                                # 4 vv 6 <<
-                                wrapx = 49
-                                wrapy = x % 50 + 150
-                                wrapangle = 180
-                        elif face == 5:
-                            if angle == 180:
-                                # 5 << 2 >>
-                                wrapx = 50
-                                wrapy = 49 - y % 50
-                                wrapangle = 0
-                            if angle == 270:
-                                # 5 ^^ 3 >>
-                                wrapx = 50
-                                wrapy = x % 50 + 50
-                                wrapangle = 0
-                        elif face == 6:
-                            if angle == 0:
-                                # 6 >> 4 ^^
-                                wrapy = 149
-                                wrapx = y % 50 + 50
-                                wrapangle = 270
-                            elif angle == 90:
-                                # 6 vv 1 vv
-                                wrapx = x + 100
-                                wrapy = 0
-                                wrapangle = 90
-                            elif angle == 180:
-                                # 6 << 2 vv
-                                wrapx = y % 50 + 50
-                                wrapy = 0
-                                wrapangle = 90
-                        wrap = (wrapx, wrapy)
-                        if wrap == (99, 150):
-                            assert True
-                        if grid[wrap]:
-                            x, y = wrap
-                            angle = wrapangle
-                            dx = int(math.cos(math.radians(angle)))
-                            dy = int(math.sin(math.radians(angle)))
+                        wrap_x, wrap_y, new_angle = transforms[(face, angle)](x, y)
+                        if grid[(wrap_x, wrap_y)]:
+                            x, y = wrap_x, wrap_y
+                            angle = new_angle
                         else:
                             break
 
@@ -228,26 +159,9 @@ def part_2(data):
     return 1000 * (y + 1) + 4 * (x + 1) + (angle % 360) // 90
 
 
-sample = """        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5"""
-
-
 def main():
     data = aocd.get_data(day=22, year=2022).splitlines()
-    # data = sample.splitlines()
-    # print(part_1(data))
+    print(part_1(data))
     print(part_2(data))
 
 
