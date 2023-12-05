b3aadfcccebfbe9826ae7ecb89ffcdadcc6546f2
amcgavin <alec.mcgavin@kogan.com.au>
2023-12-06 08:44:17 +1100

    Clean

--- 2023/python/05.py
+++ 2023/python/05.py
@@ -14,28 +14,25 @@ def chunk_iterable(iterable, size):
 def part_1(data):
     seeds = [int(x) for x in re.findall(r"\d+", data[0])]
     maps = "\n".join(data[2:]).split("\n\n")
-    paths = []
-    for i, m in enumerate(maps, 1):
+    transforms = []
+    for m in maps:
         d = m.splitlines()
-        path = []
+        transform = []
         for row in d[1:]:
             dest, source, r = [int(x) for x in re.findall(r"\d+", row)]
-            diff = source - dest
-            path.append((source, source + r, diff))
-        paths.append(sorted(path))
+            transform.append((source, source + r, source - dest))
+        transforms.append(sorted(transform))
 
     values = []
     for seed in seeds:
-        for path in paths:
-            for start, end, diff in path:
-                if start <= seed <= end:
-                    seed -= diff
-                    break
+        for transform in transforms:
+            diff = next((diff for start, end, diff in transform if start <= seed <= end), 0)
+            seed -= diff
         values.append(seed)
     return min(values)
 
 
-def part_2(data, start=0):
+def part_2(data, increment=12, start=0):
     seeds = [int(x) for x in re.findall(r"\d+", data[0])]
     valid_range = []
     for chunk in chunk_iterable(seeds, 2):
@@ -44,36 +41,37 @@ def part_2(data, start=0):
     valid_range = sorted(valid_range)
 
     maps = "\n".join(data[2:]).split("\n\n")
-    paths = []
-    for i, m in enumerate(maps, 1):
+    transforms = []
+    for m in maps:
         d = m.splitlines()
-        path = []
+        transform = []
         for row in d[1:]:
             dest, source, r = [int(x) for x in re.findall(r"\d+", row)]
-            diff = source - dest
-            path.append((source, source + r, diff))
-        paths.append(sorted(path))
+            transform.append((source, source + r, source - dest))
+        transforms.append(sorted(transform))
 
-    paths = list(reversed(paths))
+    transforms = list(reversed(transforms))
 
     location = start
     new_seed = -1
     while not any(s <= new_seed < e for s, e in valid_range):
-        location += 12
+        location += increment
         new_seed = location
-        for i, path in enumerate(paths):
-            for start, end, diff in path:
-                if start <= new_seed + diff < end:
-                    new_seed += diff
-                    break
+        for transform in transforms:
+            diff = next(
+                (diff for start, end, diff in transform if start <= new_seed + diff < end), 0
+            )
+            new_seed += diff
+
     return location
 
 
 def main():
     data = [x for x in aocd.get_data(day=5, year=2023).splitlines()]
     print(part_1(data))
-    with multiprocessing.Pool(12) as p:
-        print(min((p.starmap(part_2, [(data, i) for i in range(12)]))))
+    increment = 12
+    with multiprocessing.Pool(increment) as p:
+        print(min((p.starmap(part_2, [(data, increment, i) for i in range(12)]))))
 
 
 if __name__ == "__main__":
